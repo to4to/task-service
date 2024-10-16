103c1935afbd4d649dd48aaa1649274af450f70d
to4to <justjustifying@outlook.com>
2024-10-16 16:08:17 +0530

    feat: Implementaing Server Config to start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+
+mod api;
+
+
+use std::env::set_var;
+
+use actix_web::{HttpServer,App,web::Data,middleware::Logger};
+
+use  api::task::{
+get_task
+};
+
+#[actix_web::main]
+async fn main() ->std::io::Result<()>{
+    
+   std::env::set_var("RUST_LOG", "debug");
+   std::env::set_var("RUST_BACKTRACK","1");
+   env_logger::init();
+
+   HttpServer::new(move ||{
+      let logger=Logger::default();
+
+      App::new()
+      .wrap(logger)
+      .service(get_task)
+   }).bind(("127.0.0.1",8080))?
+   .run()
+   .await
+
 }
